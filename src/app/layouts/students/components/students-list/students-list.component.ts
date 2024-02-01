c955970fa883d78bf078902dc7fdcb1253026005
Aldo Daivd Garcia Becerril <150107593+AldoGarciaBecerril@users.noreply.github.com>
2024-02-01 13:15:30 -0600

    agrege estudiantes a la lista y agregue columna last name al students list

--- src/app/layouts/students/components/students-list/students-list.component.ts
+++ src/app/layouts/students/components/students-list/students-list.component.ts
@@ -7,7 +7,7 @@ import { Student } from '../../interface/student.interface';
   styleUrl: './students-list.component.scss'
 })
 export class StudentsListComponent {
-  displayedColumns: string[] = ['first', 'last', 'age', ];
+  displayedColumns: string[] = ['first', 'second', 'last', 'age', ];
   dataSource:Student[]=[
     {
       firstName:"Raul",
@@ -18,6 +18,46 @@ export class StudentsListComponent {
       firstName:"Jose Luis",
       lastName:"Cacharganas",
       age:23
+    },
+    {
+      firstName:"Juan",
+      lastName:"Perez",
+      age:25
+    },
+    {
+      firstName:"Rafael",
+      lastName:"Novoa",
+      age:27
+    },
+    {
+      firstName:"Raul",
+      lastName:"Gomez",
+      age:32
+    },
+    {
+      firstName:"Romina",
+      lastName:"Alcala",
+      age:20
+    },
+    {
+      firstName:"Jessica",
+      lastName:"Mondragon",
+      age:19
+    },
+    {
+      firstName:"Paloma",
+      lastName:"Aguirre",
+      age:24
+    },
+    {
+      firstName:"Alondra",
+      lastName:"Cruz",
+      age:29
+    },
+    {
+      firstName:"Debora",
+      lastName:"Melcacho",
+      age:18
     }
   ];
   constructor(){
